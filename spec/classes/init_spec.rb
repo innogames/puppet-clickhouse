69d6987d10af43c8ba26dd82d5e67fffcfbf5fce
Mikhail f. Shiryaev <mr.felixoid@gmail.com>
2018-12-12 00:35:13 +0100

    Add tests for init and server classes

---
+++ spec/classes/init_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe 'clickhouse' do
+  context 'with defaults' do
+    it { is_expected.to contain_class('clickhouse::client') }
+    it { is_expected.not_to contain_class('clickhouse::server') }
+    it { is_expected.not_to contain_class('clickhouse::repo') }
+  end
+
+  context 'with server' do
+    let(:params) { { server: true } }
+
+    it { is_expected.to contain_class('clickhouse::client') }
+    it { is_expected.to contain_class('clickhouse::server') }
+    it { is_expected.not_to contain_class('clickhouse::repo') }
+  end
+
+  context 'without client' do
+    let(:params) { { server: true, client: false } }
+
+    it { is_expected.not_to contain_class('clickhouse::client') }
+    it { is_expected.to contain_class('clickhouse::server') }
+    it { is_expected.not_to contain_class('clickhouse::repo') }
+  end
+
+  context 'manage everything' do
+    let(:params) { { server: true, client: true, manage_repo: true } }
+    let(:facts) { { os: { family: 'RedHat', release: { major: 7 } }, osfamily: 'RedHat' } }
+
+    it { is_expected.to contain_class('clickhouse::client') }
+    it { is_expected.to contain_class('clickhouse::server') }
+    it { is_expected.to contain_class('clickhouse::repo') }
+  end
+end
