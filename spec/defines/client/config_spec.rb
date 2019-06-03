158000636497717271c2bf09820d9065e9a94899
Mikhail Shiryaev <mikhail.shiryaev@innogames.com>
2019-06-03 16:34:21 +0100

    Use config.d as client configs dir

--- spec/defines/client/config_spec.rb
+++ spec/defines/client/config_spec.rb
@@ -7,29 +7,28 @@ describe 'clickhouse::client::config' do
   let(:params) { { data: { parameter: [{ subparameter: ['value'] }] } } }
 
   it do
-    is_expected.to contain_class('clickhouse::client').with(
-      conf_d_dir: '/etc/clickhouse-client/conf.d',
-    )
+    is_expected.to contain_class('clickhouse::client')
+      .with_config_d_dir('/etc/clickhouse-client/config.d')
+      .with_conf_d_dir(nil)
   end
 
   it { is_expected.to contain_package('clickhouse-client') }
 
   it do
-    is_expected.to contain_file('/etc/clickhouse-client/conf.d').with(
-      ensure: 'directory',
-      owner: 'clickhouse',
-      group: 'clickhouse',
-    )
+    is_expected.to contain_file('/etc/clickhouse-client/config.d')
+      .with_ensure('directory')
   end
 
   it do
-    out = "\
-<config>
-  <parameter>
-    <subparameter>value</subparameter>
-  </parameter>
-</config>\n"
-    is_expected.to contain_file('/etc/clickhouse-client/conf.d/custom.xml') \
-      .with_content(out)
+    is_expected.to contain_file('/etc/clickhouse-client/config.d/custom.xml')
+      .with_content(
+        <<~CONTENT
+          <config>
+            <parameter>
+              <subparameter>value</subparameter>
+            </parameter>
+          </config>
+        CONTENT
+      )
   end
 end
